0e887388820bc164bd9ab5d47afafdef6b2be7bb
martP-96 <wamarti6@cougarnet.uh.edu>
2025-04-06 17:07:41 -0500

    image add

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // load the things we need
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -9,20 +9,20 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 // index page 
-app.get('/', function(req, res) {
+app.get('/index', function(req, res) {
     var mascots = [
         { name: 'SCP Foundation Escape', difficulty:'3/10', birth_year: 2012},
         { name: 'Eery Basement Escape', difficulty:'6/10', birth_year: 1996},
         { name: 'Ikea', difficulty:'11/10', birth_year: 2013}
     ];
-    var tagline = "No programming concept is complete without a cute animal mascot.";
+    var tagline = "Here are our featured rooms!";
 
     res.render('pages/index', {
         mascots: mascots,
         tagline: tagline
     });
 });
-
+app.use(express.static('public'));
 // about page
 app.get('/about', function(req, res) {
     res.render('pages/about');
